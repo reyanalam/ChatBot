e2ef901fa8c4371f349531890d6f983862e34697
Reyan Alam <reyanalam115@gmail.com>
2024-10-28 22:19:33 +0530

    preprocessing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,17 @@
 from langchain.document_loaders import DirectoryLoader
 import os
+from src.exception import CustomeExceptionClass
+from src.loggers import logging
+import sys
+try:
+    directory = r'C:\Users\Reyan Alam\Desktop\New folder\yippee'
+    logging.info('loading the document')
+    def load_docs(directory, file_type="txt"):
+        loader = DirectoryLoader(directory, glob=f"*.{file_type}")
+        documents = loader.load()
+        logging.info('loading successfull')
+        return documents
 
-directory = r'C:\Users\Reyan Alam\Desktop\New folder\yippee'
-
-def load_docs(directory, file_type="txt"):
-    loader = DirectoryLoader(directory, glob=f"*.{file_type}")
-    documents = loader.load()
-    return documents
-
-documents = load_docs(directory, file_type="txt")
+    documents = load_docs(directory, file_type="txt")
+except Exception as e:
+            raise CustomeExceptionClass(e,sys)
