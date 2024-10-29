a65f2a3389b6c2a05fe611589c4c647e9134955a
Reyan Alam <reyanalam115@gmail.com>
2024-10-30 01:00:32 +0530

    final_commit

---
+++ src/components/preprocessing.py
@@ -0,0 +1,33 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from src.components.data_ingestion import documents
+from src.exception import CustomeExceptionClass
+from src.loggers import logging
+import sys
+from langchain.embeddings import SentenceTransformerEmbeddings
+import pinecone 
+from langchain.vectorstores import Pinecone
+
+try:
+    logging.info('splitting the documents into smaller chunks')
+    def split_docs(documents,chunk_size=500,chunk_overlap=20):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        docs = text_splitter.split_documents(documents)
+        return docs
+
+    docs = split_docs(documents)
+except Exception as e:
+            raise CustomeExceptionClass(e,sys)
+try:
+    logging.info('converting the chunks into numericals')
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+except Exception as e:
+            raise CustomeExceptionClass(e,sys)
+
+try:
+    logging.info('storing embeddings in Pinecone')
+    pinecone_api_key = ""
+    pc = pinecone.Pinecone(api_key=pinecone_api_key)
+    index_name = "chatbot"
+    index = pc.from_documents(docs, embeddings, index_name=index_name)
+except Exception as e:
+            raise CustomeExceptionClass(e,sys)
